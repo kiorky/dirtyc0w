96ca924a46eb0ee53ff07c742581161f9fa62f44
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-11-02 15:51:49 +0100

    dirtycow

--- dirtyc0w/dirtyc0w_asroot.sh
+++ dirtyc0w/dirtyc0w_asroot.sh
@@ -1,44 +1,91 @@
 #!/usr/bin/env bash
+cd $(dirname "$(readlink -f $0)")
 USAGE="\
-curl -s https://raw.githubusercontent.com/kiorky/dirtyc0w/master/dirtyc0w/dirtycow.sh>dirtycow_asroot.sh;\
+curl -s https://raw.githubusercontent.com/kiorky/dirtyc0w/master/dirtyc0w/dirtyc0w_asroot.sh>dirtyc0w_asroot.sh;\
 cat dirtyc0w_asroot.sh;\
 echo press_enter;read;sudo bash dirtyc0w_asroot.sh
 "
-set -x
-log() { echo "${0}" >&2 ; }
-tmp="$(mktemp)"
-src="$(pwd)/dirtyc0w.c"
-bin=/dirtyc0w$(basename $tmp)
-f=$tmp/m00m00
-input=m00000000000000000
-if [ ! -e "$src" ];then
-    curl \
-    https://raw.githubusercontent.com/kiorky/scripts/master/dirtyc0w/dirtyc0w.c \
-    > "$src"
+if [[ -n ${DEBUG-} ]]; then
+    set -x
 fi
+log() { echo "${@}" >&2 ; }
+vv() { echo "${@}" >&2 ; "${@}"; }
+W=$(pwd)
+URL="https://raw.githubusercontent.com/kiorky/scripts/master/dirtyc0w"
+tmp="$(mktemp)"
+srcs="pokemon.c dirtyc0w.c"
+srcs="pokemon.c"
+f=${tmp}/m00m00
+input="vuln"
+get="curl -k -s"
+if ! which gcc >/dev/null 2>&1; then
+    log "missing compiler"
+    exit 666
+fi
+if ! which curl >/dev/null 2>&1; then
+    if ! which curl >/dev/null 2>&1; then
+        get="wget --no-check-certificate -O-"
+    else
+        for i in $srcs; do
+            if [ ! -e "${i}" ]; then
+                log "missing downloader"
+                exit 666
+            fi
+        done
+    fi
+fi
+ret=0
+cd /
 if [[ -n "$tmp" ]] && [ -e "$tmp" ];then
     rm -rf "$tmp"
     mkdir "$tmp"
-    cd "$tmp"
     chmod 2777 "${tmp}"
-    rm -f "$f"
-    touch "${f}"
-    if gcc -pthread "$src" > "$bin";then
-        su -s /bin/bash -l nobody "$bin" "$f" "$input"
+    for src in ${srcs};do
+        printf hame > "${f}"
+        pattern="/${src//.c}tmp.*"
+        bin=/${src//.c}$(basename $tmp)
+        asrc=$W/$src
+        if [ ! -e "$W/$src" ];then
+            asrc="$tmp/$src"
+            $get \
+            "${URL}/${src}" \
+            2>/dev/null > "$asrc"
+        fi
+        log "Compiling ${asrc}"
+        gcc -pthread "${asrc}" -o "$bin"
+        if [ -x "$bin" ];then
+            wrapper="${bin}.sh"
+            echo "#!/usr/bin/env bash" > "${wrapper}"
+            case $bin in
+                *pokemon*)
+                    echo '"'$bin'" "'$f'" "'$input'"' >> "${wrapper}"
+                    ;;
+                *dirtyc0w*)
+                    echo '"'$bin'" "'$f'" "'$input'"' >> "${wrapper}"
+                    ;;
+            esac
+            chmod +x "${wrapper}"
+            su -s /bin/bash -l nobody -c "${wrapper}"
+        else
+            log "noncompiled: ${src}"
+            if [ $ret -lt 1 ]; then
+                ret=3
+            fi
+        fi
+        if [[ -z ${NODELETE-} ]];then
+            rm -vf $pattern $spattern
+        fi
         if grep -q "$input" "$f";then
             log vuln
             ret=455
-        else
-            log nonvuln
-            ret=0
+            break
         fi
-    else
-        log noncompiled
-        ret=3
-    fi
+    done
 else
     ret=1
 fi
-rm -rf "${bin}" "${tmp}"
+if [[ -z ${NODELETE-} ]];then
+    rm -rf "${tmp}"
+fi
 exit $ret
 # vim:set et sts=4 ts=4 tw=80:

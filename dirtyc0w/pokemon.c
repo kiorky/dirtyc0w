dc60ffa3d7cc57b783725f75c76168cbd5557e38
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-11-02 20:19:25 +0100

    Dirty cow

--- dirtyc0w/pokemon.c
+++ dirtyc0w/pokemon.c
@@ -62,3 +62,4 @@ int main(int argc,char *argv[])            {// entrypoint
   return 0                                 ;// return
                                            }// / entrypoint
 //////////////////////////////////////////////
+//
